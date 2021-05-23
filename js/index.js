7494ef9b04e854fd29904a1cab1eb1fd88f6fdba
Dmitriy <dmitriypruss21041984@gmail.com>
2021-05-23 11:40:23 +0300

    4 commit

--- js/index.js
+++ js/index.js
@@ -42,38 +42,31 @@ console.groupEnd();
 
 // 1.2 Получить массив полных имен лиц женского пола школьного возраста (6 - 18 лет).
 console.groupCollapsed('Task 1.2');
-    const teenageWomenFullnames = users.filter(getTeenageWomenFullnames);
+    const teenageWomenFullnames = users.filter(areTeenageWomen).map(getFullnames);
     console.table(teenageWomenFullnames);
 
-    function getTeenageWomenFullnames(user) {
-        if (!user.isMale && user.age >= 6 && user.age <= 18) {
-            return user.getFullName();
-        };
+    function areTeenageWomen(user, i, arr) {
+        return !user.isMale && user.age >= 6 && user.age <= 18;
+    };
+
+    function getFullnames(user, i, arr) {
+        return user.getFullName();
     };
 console.groupEnd();
 
 // 1.3 Проверить, есть ли среди пользователей пользователь с email`ом useremail99@gmail.com
 console.groupCollapsed('Task 1.3');
     console.log('users.some(isSomeMail("useremail99@gmail.com")) :>> ', users.some(
-        function(i, index) {
-            return isSomeMail(i, index, "useremail99@gmail.com");
-        })
-    );
-
-    function isSomeMail(i, index, maleName) {
-        if (i.email === maleName) {
-            console.log(`users[${index}] = ${i.email}`);
-        };
-        return i.email === maleName;
-    };
+        item => (item.email === "useremail99@gmail.com")
+    ));
 console.groupEnd();
 
 // 1.4 Проверить, все ли пользователи подписаны (subscribed).
 console.groupCollapsed('Task 1.4');
     console.log('users.every(isSubscribed) :>> ', users.every(isSubscribed));
 
-    function isSubscribed(i) {
-        return i.isSubscribed;
+    function isSubscribed(item) {
+        return item.isSubscribed;
     };
 console.groupEnd();
 
